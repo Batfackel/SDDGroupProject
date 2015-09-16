91c04bfc05df209c19e12c21c557becde3dae0c2
Younouss <younoussthiam@live.com>
2015-09-16 06:08:14 -0500

    Update Main.java

--- SDD Project/src/Controller/Main.java
+++ SDD Project/src/Controller/Main.java
@@ -5,8 +5,6 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.text.SimpleDateFormat;
@@ -17,7 +15,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class Main extends JFrame implements ActionListener, MouseListener, KeyListener {
+public class Main extends JFrame implements ActionListener, MouseListener {
 
     private GamePanel gamePanel;
     private final GameData gameData;
@@ -30,10 +28,13 @@ public class Main extends JFrame implements ActionListener, MouseListener, KeyLi
     private int screenHeight = 1000;
     private JLabel lbl;
     private Date now = new Date();
-    private TempShip ship;
+    public  TempShip ship;
     private JButton leaderBoard;
     private LeaderBoard leaderPanel;
     private Container c;
+    public TempShip Ship(){
+        return this.ship;
+    }
     public Main() {
         //changed sizing to fit the default image
         setSize(screenWidth, screenHeight);
@@ -66,7 +67,7 @@ public class Main extends JFrame implements ActionListener, MouseListener, KeyLi
         gamePanel.addMouseListener(this);
         startButton.setFocusable(false); // "Start" button click should not receive keyboard data
         gamePanel.setFocusable(true); // receives keyboard data
-        gamePanel.addKeyListener(this);
+        gamePanel.addKeyListener(new KeyController());
         startButton.addActionListener(this);
         quitButton.addActionListener(this);
         leaderBoard.addActionListener(this);
@@ -142,55 +143,12 @@ public class Main extends JFrame implements ActionListener, MouseListener, KeyLi
         }
     }
     
-    @Override
-    public void keyPressed(KeyEvent ke) {
-        switch (ke.getKeyCode()) {
-            case KeyEvent.VK_LEFT:
-                ship.moveLeft();                
-                break;
-            case KeyEvent.VK_RIGHT:
-                ship.moveRight();                
-                break;
-            case KeyEvent.VK_UP:
-                ship.moveUp();              
-                break;
-            case KeyEvent.VK_DOWN:
-                ship.moveDown(); 
-                break;
-            case KeyEvent.VK_1:
-                ship.setRateOfSpeed(5);
-                ship.setShipType(1);
-                break;
-            case KeyEvent.VK_2:
-                ship.setRateOfSpeed(8);
-                ship.setShipType(2);
-                break;
-            case KeyEvent.VK_3:
-                ship.setRateOfSpeed(12);
-                ship.setShipType(3);
-                break;
-            case KeyEvent.VK_4:
-                ship.setRateOfSpeed(15);
-                ship.setShipType(4);
-                break;
-            case KeyEvent.VK_5:
-                ship.setRateOfSpeed(18);
-                ship.setShipType(5);
-                break;
-            case KeyEvent.VK_6:
-                ship.setRateOfSpeed(21);
-                ship.setShipType(6);
-                break;
-        }
-    }
-    
     public void setScreenSize(int w, int h){
         this.screenHeight = h;
         this.screenWidth = w;
         setSize(screenWidth, screenHeight);
     }
-    
-    private void startTimer(){
+    private void startTimer(){ 
 		now.setHours(0);
 		now.setMinutes(0);
 		now.setSeconds(0);
@@ -206,7 +164,6 @@ public class Main extends JFrame implements ActionListener, MouseListener, KeyLi
                 timer.start();
     }
     
-   
     @Override
     public void mouseClicked(MouseEvent me) {
     }
@@ -223,13 +180,6 @@ public class Main extends JFrame implements ActionListener, MouseListener, KeyLi
     public void mouseExited(MouseEvent me) {
     }
     //move into controller that handles player input
-    @Override
-    public void keyTyped(KeyEvent ke) {
-    }
-    //move into controller that handles player input
-    @Override
-    public void keyReleased(KeyEvent ke) {
-    }
     
     //initial main not needed
     /*
