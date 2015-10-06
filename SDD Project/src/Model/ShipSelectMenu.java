870db39cdb2a070ba044da6d4c7cb42846e98d3f
Will McLain <wmclain@uco.edu>
2015-10-06 14:09:33 -0500

    Selection screen cont...
    Fleshing out the ship selection screen.

--- SDD Project/src/Model/ShipSelectMenu.java
+++ SDD Project/src/Model/ShipSelectMenu.java
@@ -5,6 +5,7 @@
  */
 package Model;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.io.File;
@@ -28,11 +29,32 @@ private Image backgroundImage = null;
    private final static int FOURTH_SHIP_SELECTED = 4;
    private final static int FIFTH_SHIP_SELECTED = 5;
    private final static int SIXTH_SHIP_SELECTED = 6;
+   //Resolutions Available to deterimne which screen to use.
+   private final static int RESOLUTION_800X1000 = 1;
+   private final static int RESOLUTION_1280X720 = 2;
+   private final static int RESOLUTION_1400X900 = 3;
+   //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+   //Values for rectangles on the secetion screen at 800X1000 Res
+   private final static int TOP_COL_OF_DEFAULT_SHIP_X_VALUE_800X1000 = 90;  //YELLOW SHIP
+   private final static int TOP_COL_OF_SHIP_V_X_VALUE_800X1000 = 335;  //DARK GREEN SHIP
+   private final static int TOP_COL_OF_SHIP_X_X_VALUE_800X1000 = 565;  //Violet SHIP
+   private final static int TOP_ROW_OF_SHIPS_Y_VALUE_800X1000 = 365;  
+   private final static int TOP_COL_OF_SHIP_W_X_VALUE_800X1000 = 90;  //SHADOW SHIP
+   private final static int TOP_COL_OF_ShipV_X_VALUE_800X1000 = 335;  //MINT SHIP
+   private final static int TOP_COL_OF_ShipX_X_VALUE_800X1000 = 565;  //SUN SHIP
+   private final static int BOTTOM_ROW_OF_SHIPS_Y_VALUE_800X1000 = 565;
+   private final static int SELECTION_RECTANGLE_WIDTH_800X1000 = 150;
+   private final static int SELECTION_RECTANGLE_HEIGHT_800X1000 = 0;
+   //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
    private int currentSelection;
+   private int currentResolution;
    private int state;
    private float x, y;
-public ShipSelectMenu()
+public ShipSelectMenu(int currentResolution)//Acccepts 1-3 to determine current screen Res
 {
+    this.currentResolution = currentResolution;
+    this.x = 0;
+    this.y = 0;
     state = FIRST_SHIP_SELECTED;
     currentSelection = FIRST_SHIP_SELECTED;
     backgroundImage = getImage(imagePath + separator + "images" + 
@@ -97,9 +119,22 @@ public ShipSelectMenu()
 //-------------------------------------------------------------------------   
     @Override
     public void render(Graphics g) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+        g.drawImage(backgroundImage, (int) x, (int) y, null);
+        switch (currentResolution) {
+            case RESOLUTION_800X1000: {
+                switch (currentSelection) {
+                    case 1: {
+                        g.setColor(Color.black);
 
+                        g.drawRect(TOP_COL_OF_DEFAULT_SHIP_X_VALUE_800X1000, TOP_ROW_OF_SHIPS_Y_VALUE_800X1000,
+                                SELECTION_RECTANGLE_WIDTH_800X1000, SELECTION_RECTANGLE_HEIGHT_800X1000);
+                        //  g.drawRect((int)this.x, (int)this.y, (int)this.shipWidth, (int)this.shipHeight);
+                        break;
+                    }
+                }
+            }
+        }
+    }
     @Override
     public void update() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
