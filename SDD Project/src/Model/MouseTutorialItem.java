ee4c61fcf414d9dc62cef4bab75f871faa34c5f1
Yuchen <fengyuchen0830@gmail.com>
2015-10-10 13:13:44 -0500

    tutorial image transparent,pause resume button working good
    tutorial image transparent,pause resume button working good

--- SDD Project/src/Model/MouseTutorialItem.java
+++ SDD Project/src/Model/MouseTutorialItem.java
@@ -10,9 +10,13 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.InputEvent;
 import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 /**
  *
@@ -20,9 +24,15 @@ import java.awt.event.MouseEvent;
  */
 public class MouseTutorialItem implements ITutorialItem{
 
-    public MouseTutorialItem(String text){
+    public MouseTutorialItem(String text) throws IOException{
         this.text = text;
         this.isHandled = false;
+        
+        String imagePath = System.getProperty("user.dir");
+        // separator: Windows '\', Linux '/'
+        String separator = System.getProperty("file.separator");
+        this.text = imagePath + separator + "images" + separator + this.text;
+        this.image = ImageIO.read(new File(this.text));
     }
     
     @Override
@@ -37,16 +47,17 @@ public class MouseTutorialItem implements ITutorialItem{
     @Override
     public void display(Graphics g) {
         if(!isHandled){
-            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-            g.setFont(new Font("TimesRoman",Font.BOLD,40));
-            g.setColor(Color.red);
+            g.drawImage(image, 200, 550, null);
+            //Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+            //g.setFont(new Font("TimesRoman",Font.BOLD,40));
+            //g.setColor(Color.red);
               
-            g.drawString(text, (int)(85), (int)(600));  
+            //g.drawString(text, (int)(85), (int)(600));  
             //g.drawString(text, (int)(screenSize.getWidth()/ 2), (int)(screenSize.getHeight() / 2));  
         }
     }
     
     private String text;
     private boolean isHandled;
-    
+    private Image image;
 }
--- SDD Project/src/View/GamePanel.java
+++ SDD Project/src/View/GamePanel.java
@@ -11,10 +11,16 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import javax.imageio.ImageIO;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class GamePanel extends JPanel {
       
@@ -30,6 +36,8 @@ public class GamePanel extends JPanel {
      private HUD hud;
     public ShipFactory shipMaker;
     private Image titleScreen;
+    private Date now; 
+    private Timer timer;
 
     public GamePanel(Animator animator, GameData gameData) {
         this.animator = animator;
@@ -58,12 +66,44 @@ public class GamePanel extends JPanel {
         hud = new HUD();
     }
 
-    public void startGame() {
+    public void startGame(final JLabel lbl) {
        
         Thread t = new Thread(animator);
         t.start();
+        
+        now = new Date();
+        startTimer(lbl);
     }
 
+    public void pauseGame(){
+        animator.setPause(true);
+        if (timer != null) {
+            timer.stop();
+        }
+    }
+    
+    public void resumeGame(){
+        animator.setPause(false);
+        if (timer != null) {
+            timer.start();
+        }
+    }
+    
+        private void startTimer(final JLabel lbl){ 
+		now.setHours(0);
+		now.setMinutes(0);
+		now.setSeconds(0);
+		timer = new Timer(1000, new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Date now2 = new Date(now.getTime() + 1000);
+				now = now2;
+				SimpleDateFormat formatter = new SimpleDateFormat("HH:mm:ss");
+				lbl.setText(formatter.format(now));
+			}
+		});
+                
+                timer.start();
+    }
     
     
     
