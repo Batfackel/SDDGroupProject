8030517f7cd1141e40d02c4df8622492b8b660e5
Brian Davis <unloadedcrayon@gmail.com>
2015-10-29 12:07:22 -0500

    Added untracked files

---
+++ SDD Project/src/Model/Sound.java
@@ -0,0 +1,60 @@
+package Model;
+
+import java.applet.Applet;
+import java.applet.AudioClip;
+
+public class Sound {
+
+    public static final Sound sound1 = new Sound("/sounds//frantic.wav");
+    public static final Sound shot = new Sound("/sounds//shot.wav");
+    public static final Sound shot2 = new Sound("/sounds//laser.wav");
+    public static final Sound shot3 = new Sound("/sounds//teleport.wav");
+    public static final Sound dead = new Sound("/sounds//explosion.wav");
+    private AudioClip clip;
+
+    public Sound(String fileName) {
+
+        try {
+            clip = Applet.newAudioClip(Sound.class.getResource(fileName));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loop() {
+        try {
+            new Thread() {
+                public void run() {
+                    clip.loop();
+                }
+            }.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void play() {
+        try {
+            new Thread() {
+                public void run() {
+                    clip.play();
+                }
+            }.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void stop() {
+        try {
+            new Thread() {
+                public void run() {
+                    clip.stop();
+                }
+            }.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
+
