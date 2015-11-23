785a0ad9d836354391e1e4ca76c79ed6841212c9
Michael McGregor <mcgregor9033@yahoo.com>
2015-11-23 00:31:05 -0600

    weapon factory scrub
    new items need to have unique id numbers

--- SDD Project/src/Model/WeaponPowerFactory.java
+++ SDD Project/src/Model/WeaponPowerFactory.java
@@ -1,40 +1,40 @@
-/*
- * Part of the factory design pattern that is intended to create weapon power
- * ups when enemy ships are destroyed. 
- */
 package Model;
 
 /**
- *
+ * Part of the factory design pattern that is intended to create weapon power
+ * ups when enemy ships are destroyed.
+ * 
  * @author Michael McGregor
  */
 public class WeaponPowerFactory {
-        //Default constructor  
-  private AbstractItem leItem;   
-  
-  void AbstractItem() {
-     leItem = null; 
-  }
-  
-  public AbstractItem getWeapon(String weaponSelection, float x, float y) {
-        if(weaponSelection.equalsIgnoreCase("KINETIC")) {
+
+    //Default constructor  
+
+    private AbstractItem leItem;
+
+    void AbstractItem() {
+        leItem = null;
+    }
+
+    public AbstractItem getWeapon(String weaponSelection, float x, float y) {
+        if (weaponSelection.equalsIgnoreCase("KINETIC")) {
             leItem = new KineticWeapon(x, y, 0);
             return leItem;
-        }
-        else if(weaponSelection.equalsIgnoreCase("LASER")) {
+        } 
+        else if (weaponSelection.equalsIgnoreCase("LASER")) {
             leItem = new LaserWeapon(x, y, 1);
             return leItem;
-        }
-        else if(weaponSelection.equalsIgnoreCase("MISSILE")) {
+        } 
+        else if (weaponSelection.equalsIgnoreCase("MISSILE")) {
             leItem = new MissileWeapon(x, y, 2);
             return leItem;
         }
-        if(weaponSelection.equalsIgnoreCase("HEALTH")) {
-            leItem = new HealthItem(x, y, 0);
+        else if (weaponSelection.equalsIgnoreCase("HEALTH")) {
+            leItem = new HealthItem(x, y, 10); //numer is reference and needs to be unique
             return leItem;
-        }
+        } 
         else {
             throw new IllegalArgumentException("NO SUCH WEAPON");
         }
-    }    
+    }
 }
